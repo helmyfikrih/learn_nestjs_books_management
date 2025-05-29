de9c8ebbedb14db60ce45d8f66f0c75091c06ef0
Helmy Fikri Husein <helmyfikrih@gmail.com>
2025-05-29 10:39:02 +0700

    using role guards

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,12 @@
-import { Body, Controller, Post, Request, Get, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post, Request, Get, UseGuards, HttpCode, HttpStatus } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
 import { User } from './entities/user.entity';
 import { AuthGuard } from './guard/auth.guard';
+import { RolesGuard } from './guard/role.guard';
+import { Roles } from './decorators/roles.decorator';
+import { Role } from './enum/role.enum';
 
 @Controller('auth')
 export class AuthController {
@@ -13,10 +16,11 @@ export class AuthController {
     }
 
     @Post('register')
-    async register (@Body() registerDto: RegisterDto) {
+    async register(@Body() registerDto: RegisterDto) {
         return await this.authService.registerUser(registerDto)
     }
 
+    @HttpCode(HttpStatus.OK)
     @Post('login')
     async login(@Body() loginDto: LoginDto) {
         return await this.authService.loginUser(loginDto)
@@ -24,7 +28,16 @@ export class AuthController {
 
     @UseGuards(AuthGuard)
     @Get('getUser')
-    async getUser(@Request() request): Promise<User | null>{
+    async getUser(@Request() request): Promise<User | null> {
         return await this.authService.getUser(request.user.id)
-    } 
+    }
+
+    @UseGuards(AuthGuard, RolesGuard)
+    @Roles(Role.ADMIN)
+    @Get('test')
+    getTest(): { message: string } {
+        return {
+            message: "Test role guard berhasil"
+        }
+    }
 }
